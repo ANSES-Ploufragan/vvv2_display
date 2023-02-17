2a1b1aecd6e40fa255053e9dbffaeb7127610123
f.touzain <fabrice.touzain@anses.fr>
2023-02-17 14:08:49 +0100

    improve legends/title pos

--- src/visualize_snp_v4.R
+++ src/visualize_snp_v4.R
@@ -14,23 +14,35 @@
 
 library(ggplot2) # import the ggplot2 library
 args <- commandArgs(TRUE) # all arguments are character types
-density = read.table(args[1], h=T, sep = "\t") # read the dataframe
+
+density <- try( read.table(args[1], h=T, sep = "\t") ) # read the dataframe
+if(inherits(density,"try-error"))
+  density <- NULL
+
 threshold = as.numeric(args[2]) # define threshold
 
 t1 = as.character(threshold) # define threshold as a character
 t = paste("Nucleotide Variation - threshold = ", t1, sep = ' ')
 
-p = ggplot(density) # plot initialization
-p1 = p + geom_point(aes(x = position, y = 0, color = gene_id), size = 1, show.legend = F, shape = 15) # add the consensus points, and remove the legend "size"
+if(is.null(density)){ # means no snp found
+  p = ggplot() # plot initialization
+}else{
+  p = ggplot(density) # plot initialization
+}
+
+p1 = p + geom_point(aes(x = position, y = 0, color = gene_id), size = 1, show.legend = F, shape = 15) # add the consensus points, and remove the legend "size"  
 p2 = p1 + geom_point(aes(x = position, y = as.numeric(variant_percent), color = gene_id)) # add the variant points
 p3 = p2 + geom_line(aes(x=position, y=threshold)) # add the threshold line
-p4 = p3 + labs(list(title = t, x = "Base Position", y = "Variant Frequency")) # add axes and graph titles
-p5 = p4 + theme(legend.position = "bottom") # modify the legend position
-p6 = p5 + ylim(-0.06,1.2) # modify the scale
-p7 = p6 + geom_text(aes(x = position, y = variant_percent + 0.01, label = indice, angle = 0)) # add indice to the grapĥ
-#p8 = p7 + geom_text(x = 0, y = threshold + 0.01, label = t) # add the threshold text
-#p9 = p8 + geom_line(aes(x = position, y = 0.5), color = "red") 
-p8 = p7 + geom_line(aes(x = position, y = 0.5), color = "red") 
+p4 = p3 + labs(title = t) # add graph title
+p5 = p4 + xlab("Base Position") # add x axe title
+p6 = p5 + ylab("Variant Frequency") # add axes and graph titles
+p7 = p6 + theme(legend.position = "bottom") # modify the legend position
+p8 = p7 + ylim(-0.06,1.2) # modify the scale
+p9 = p8 + geom_text(aes(x = position, y = variant_percent + 0.01, label = indice, angle = 0)) # add indice to the grapĥ
+#p10 = p9 + geom_text(x = 0, y = threshold + 0.01, label = t) # add the threshold text
+#p11 = p10 + geom_line(aes(x = position, y = 0.5), color = "red") 
+p10 = p9 + geom_line(aes(x = position, y = 0.5), color = "red")
+p11 = p10 + theme(plot.title = element_text(hjust=0.5))
 
 
 ggsave(args[3], plot = last_plot(), width = 20, dpi = 600) # save the graph
