f37ecab50cc82457077838dd61b20ec0b2a6a587
f.touzain <fabrice.touzain@anses.fr>
2023-12-20 14:03:37 +0100

    var name changed for homogeneity, file names too

--- src/correct_covdepth_f.py
+++ src/correct_covdepth_f.py
@@ -30,19 +30,19 @@ import inspect
 frame = inspect.currentframe()
 
 # debug
-b_test_correct_covdepth_f = False #  2023 12 19
+b_test_cov_depth_corr_f = False #  2023 12 19
 b_test = False
 
 prog_tag = '[' + os.path.basename(__file__) + ']'
 
-covdepth_f    = '' # in, covdepth file provided by samtools depth -aa file.bam
-correct_covdepth_f = '' # out same as previous but positions on contigs are shifted according to previous contigs
+cov_depth_f    = '' # in, cov_depth file provided by samtools depth -aa file.bam
+cov_depth_corr_f = '' # out same as previous but positions on contigs are shifted according to previous contigs
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--covdepth_f", dest='covdepth_f',
+parser.add_argument("-s", "--cov_depth_f", dest='cov_depth_f',
                     help="in: coverage depth file provided by samtools -aa in.bam (all contigs)",
                     metavar="FILE")
-parser.add_argument("-c", "--correct_covdepth_f", dest='correct_covdepth_f',
+parser.add_argument("-c", "--cov_depth_corr_f", dest='cov_depth_corr_f',
                     help="out: coverage depth file corrected for position (cumulative position in case of several contigs)",
                     metavar="FILE")
 parser.add_argument("-z", "--test_all", dest='b_test_all',
@@ -62,10 +62,10 @@ args = parser.parse_args()
 b_test_all = args.b_test_all
 
 if b_test_all:
-    b_test_correct_covdepth_f = True
+    b_test_cov_depth_corr_f = True
     b_test = True
 else:
-    b_test = b_test_correct_covdepth_f
+    b_test = b_test_cov_depth_corr_f
 
 if ((not b_test)and
     ((len(sys.argv) < 2) or (len(sys.argv) > 10))):
@@ -75,30 +75,30 @@ if ((not b_test)and
     sys.exit(0)
 
 # print('args:', args)
-if args.covdepth_f is not None:
-    covdepth_f = os.path.abspath(args.covdepth_f)
+if args.cov_depth_f is not None:
+    cov_depth_f = os.path.abspath(args.cov_depth_f)
 elif(not b_test):
-    sys.exit(prog_tag + "[Error] You must provide covdepth_f")
-if args.correct_covdepth_f is not None:
-    correct_covdepth_f = os.path.abspath(args.correct_covdepth_f)
+    sys.exit(prog_tag + "[Error] You must provide cov_depth_f")
+if args.cov_depth_corr_f is not None:
+    cov_depth_corr_f = os.path.abspath(args.cov_depth_corr_f)
 elif(not b_test):
-    sys.exit(prog_tag + "[Error] You must provide correct_covdepth_f")
+    sys.exit(prog_tag + "[Error] You must provide cov_depth_corr_f")
 
 if args.b_verbose is not None:
     b_verbose = args.b_verbose
 
-if b_test_correct_covdepth_f:
+if b_test_cov_depth_corr_f:
     test_dir = '../test_vvv2_display/'
     test_name = [
         'res'
         # 'res2'
     ]
     for resn in test_name:
-        covdepth_f         = test_dir + "res_vvv2_covdepth.txt"
-        correct_covdepth_f = test_dir + "res_vvv2_covdepth_corrected.txt"
+        cov_depth_f         = test_dir + "res2_covdepth.txt"
+        cov_depth_corr_f = test_dir + "res2_covdepth_corrected.txt"
         cmd = ' '.join(['./correct_covdepth_f.py',
-                        "-s", covdepth_f,
-                        "-c", correct_covdepth_f
+                        "-s", cov_depth_f,
+                        "-c", cov_depth_corr_f
                         ])
         if b_verbose:
             cmd = cmd + " -v"
@@ -111,9 +111,9 @@ if b_test_correct_covdepth_f:
 # ----------------------------------------------------------------------
 # reads seqstat file to get info on genome_length and on contigs lengths
 # ----------------------------------------------------------------------
-print(prog_tag + " Reads "+covdepth_f+" file")
-print(prog_tag + " Creates "+correct_covdepth_f+" file")
-o = open(correct_covdepth_f, 'w+')
+print(prog_tag + " Reads "+cov_depth_f+" file")
+print(prog_tag + " Creates "+cov_depth_corr_f+" file")
+o = open(cov_depth_corr_f, 'w+')
 
 # init var
 prev_contig_nr = 1
@@ -121,13 +121,13 @@ contig_num   = 0
 
 pos = 0
 curr_shift = 0
-with open(covdepth_f) as cdf:
+with open(cov_depth_f) as cdf:
     for line in cdf:
         line_fields = line.split("\t")
         # print("line_fields:"+','.join(line_fields))
         contig_nr = line_fields[0]
         contig_pos = line_fields[1]
-        covdepth = line_fields[2]
+        cov_depth = line_fields[2]
         # print("line_fields:"+','.join(line_fields))
         if b_verbose:
             print(prog_tag + " contig:"+contig_name+" length:"+str(contig_length))
@@ -140,12 +140,12 @@ with open(covdepth_f) as cdf:
         pos = curr_shift + int(contig_pos)
         pos_str = str(pos)
         # write pos and related coverage depth
-        o.write("\t".join([pos_str, covdepth]))
+        o.write("\t".join([pos_str, cov_depth]))
         if b_verbose:            
-            print("record pos:",pos_str,", covdepth:", covdepth)
+            print("record pos:",pos_str,", cov_depth:", cov_depth)
 
         prev_contig_nr = contig_nr
 cdf.close()
 o.close()
 
-print(prog_tag + ' '+ correct_covdepth_f + " file created")
+print(prog_tag + ' '+ cov_depth_corr_f + " file created")
