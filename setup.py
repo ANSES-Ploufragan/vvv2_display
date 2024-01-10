37c8691589880b456e4db97ff984af3b510dd89e
f.touzain <fabrice.touzain@anses.fr>
2024-01-10 15:07:52 +0100

    update R dependencies

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ setuptools.setup(
         "r-ggplot2==3.6.6",
         "r-gridextra==2.3",
         "r-cowplot==1.1.1",
-        "conda-forge::r-ggh4x",
+        "conda-forge::r-stringr=1.5.1",
         "pysam==0.19.1",
         "numpy==1.23.1"],  # Optional
     scripts=[
